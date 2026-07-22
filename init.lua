08b5c3b03b56aaa5894c08138c1758a18d8b8eb6
etrechyp <e.trechy.p@gmail.com>
2026-07-22 17:24:18 -0400

    Configurada IP local y servidor en init.lua

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
 -- this is the first file executed when the application starts
 -- we have to load the first modules form here
 
--- updater
+-- updater and services config
 Services = {
-    --updater = "http://localhost/api/updater.php", --./updater
-    --status = "http://localhost/login.php", --./client_entergame | ./client_topmenu
-    --websites = "http://localhost/?subtopic=accountmanagement", --./client_entergame "Forgot password and/or email"
-    --createAccount = "http://localhost/clientcreateaccount.php", --./client_entergame -- createAccount.lua
-    --getCoinsUrl = "http://localhost/?subtopic=shop&step=terms", --./game_market
+    updater = "http://192.168.10.164:8000/api/updater.php",
+    status = "http://192.168.10.164:8000/login.php",
+    websites = "http://192.168.10.164:8000/?subtopic=accountmanagement",
+    createAccount = "http://192.168.10.164:8000/clientcreateaccount.php",
+    getCoinsUrl = "http://192.168.10.164:8000/?subtopic=shop&step=terms",
     clientAssets = {
-        enabled = true,
+        enabled = false, -- Desactivado para evitar descargas externas durante pruebas locales
         repository = "dudantas/tibia-client",
         installSounds = true,
         strictManifestSha256 = true,
@@ -20,86 +20,29 @@ Services = {
         installArchiveExtras = true,
         archiveExtraPrefixes = { "bin" },
         installPackagedFiles = true
-    }, -- ./client_assets
+    },
 }
 
 --- Enables or disables the entire server configuration block.
--- Set to `false` to disable all configuration below.
 local ENABLE_SERVERS = true
-
----
--- @module Servers_init
--- Configuration table for all servers used by the system.
---
--- This entire block is conditionally enabled based on ENABLE_SERVERS.
--- When ENABLE_SERVERS == false, everything is ignored/disabled.
---
-
----
--- Server configuration system for multi-server or multi-world clients.
---
--- This structure allows a single client build to connect to multiple servers
--- without requiring duplicate client folders.
---
--- A server that hosts several worlds, or that provides a separate test environment,
--- can simply define additional entries inside this configuration table.
---
--- Instead of maintaining multiple client installations (one per world/server),
--- the client can switch between servers by selecting the desired configuration entry.
--- This simplifies testing, avoids redundant directories, and centralizes connection settings.
---
--- The ENABLE_SERVERS flag allows the entire configuration block to be enabled or disabled
--- without deleting or commenting out individual entries.
---
 
 Servers_init = {}
 
 if ENABLE_SERVERS then
-
-    ---
-    -- List of servers and their configuration parameters.
-    -- Each entry defines port, protocol, and authentication options.
-    -- @table Servers_init
-    --
     Servers_init = {
-
-        -- Local login server
-        ---
-        -- Configuration for local login server.
-        -- @class table
-        -- @name local_login
-        -- @field port Port used for HTTP connection
-        -- @field protocol Protocol identifier used by the application
-        -- @field httpLogin Enables HTTP-based login on the server
-        -- @field useAuthenticator Enables additional authentication layer
-        --
-        ["http://127.0.0.1/login.php"] = {
-            port = 80,
-            protocol = 1511,
+        -- Servidor local apuntando a tu MyAAC (Protocolo 13.10)
+        ["http://192.168.10.164:8000/login.php"] = {
+            port = 8000,
+            protocol = 1310,
             httpLogin = true,
             useAuthenticator = false
-        },
-
-        -- External server
-        ---
-        -- Configuration for external server ip.net.
-        -- @class table
-        -- @name ip_net
-        -- @field port TCP port used for connection
-        -- @field protocol Protocol identifier used by the server
-        -- @field httpLogin Indicates if the server allows HTTP login
-        --
-        ["ip.net"] = {
-            port = 7171,
-            protocol = 860,
-            httpLogin = false
         }
     }
 end
 
-g_app.setName("OTClient - Redemption");
-g_app.setCompactName("otclient");
-g_app.setOrganizationName("otcr");
+g_app.setName("Zephyr OT");
+g_app.setCompactName("zephyrot");
+g_app.setOrganizationName("zephyrot");
 
 g_app.hasUpdater = function()
     return (Services.updater and Services.updater ~= "" and g_modules.getModule("updater"))
@@ -177,9 +120,6 @@ local function loadModules()
     if g_resources.fileExists(script) then
         dofile(script)
     end
-
-    -- uncomment the line below so that modules are reloaded when modified. (Note: Use only mod dev)
-    -- g_modules.enableAutoReload()
 end
 
 -- run updater, must use data.zip
